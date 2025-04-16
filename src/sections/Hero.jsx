640ed43384e0feb71409c1a0776951f709fe7db6
Michael Anthony <ukomikey@gmail.com>
2025-04-15 23:46:03 -0400

    Added a dynamic CTA button to be reused through out the project

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import {words} from "../constants/index.js";
+import Button from "../components/Button.jsx";
 
 const Hero = () => {
     return (
@@ -33,6 +34,14 @@ const Hero = () => {
                             <h1>into Real Projects</h1>
                             <h1>that Deliver Results</h1>
                         </div>
+                        <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+                            Hi, I'm Michael, a developer based in Canada with a passion for code.
+                        </p>
+                        <Button
+                          className="md:w-80 md:h-16 w-60 h-12"
+                          id="button"
+                          text="See my Work"
+                        />
                     </div>
                 </header>
                 {/*RIGHT: 3D MODEL */}
