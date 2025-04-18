23e551a165262b90a8b1946204a37a76b461ac9b
Michael Anthony <ukomikey@gmail.com>
2025-04-18 12:55:17 -0400

    Added Animated counter to the Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,27 @@
 import {words} from "../constants/index.js";
 import Button from "../components/Button.jsx";
 import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { useGSAP} from "@gsap/react";
+import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.fromTo('.hero-text h1',
+            {
+                y: 50,
+                opacity: 0
+            },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: 0.2,
+                duration: 1,
+                ease: 'power2.inOut'
+            }
+        )
+    })
+
     return (
         <section id="hero" className="relative overflow-hidden">
             <div className="absolute top-0 left-0 z-10">
@@ -19,7 +38,7 @@ const Hero = () => {
                                 <span className="slide">
                                   <span className="wrapper">
                                       {words.map((word) => (
-                                          <span key={word.text} className="flex items-center gap-3 gap-1 pb-2">
+                                          <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
                                               <img
                                                 src={word.imgPath}
                                                 alt={word.text}
@@ -52,6 +71,7 @@ const Hero = () => {
                     </div>
                 </figure>
             </div>
+            <AnimatedCounter />
         </section>
     )
 }
