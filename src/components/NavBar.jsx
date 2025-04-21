685e0239033d1a15f7c1534150ed85c43af9fdb2
Michael Anthony <ukomikey@gmail.com>
2025-04-21 10:00:38 -0400

    Added the ExperienceSection alongside the animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,7 +19,7 @@ const NavBar = () => {
         <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
             <div className="inner">
                 <a className="logo" href="#hero">
-                    Michael | A
+                    Michael.A
                 </a>
 
                 <nav className="desktop">
